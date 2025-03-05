92151725b77c23f4c9f500a424aa479146f90d58
Max86 <emmanuel@max86.com>
2025-03-05 17:16:14 +0100

    Homepage in React

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
+import './assets/css/style.css'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Main from './components/Main.jsx'
+
+
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header />
+    <Main />
+    <Footer />
     </>
   )
 }
